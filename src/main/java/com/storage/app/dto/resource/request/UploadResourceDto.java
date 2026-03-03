773c0d89063a1da11d08ad86c6233a2a02ec852c
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-03 08:50:39 +0300

    update UploadResourceDto

--- src/main/java/com/storage/app/dto/resource/request/UploadResourceDto.java
+++ src/main/java/com/storage/app/dto/resource/request/UploadResourceDto.java
@@ -1,18 +1,11 @@
 package com.storage.app.dto.resource.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UploadResourceDto {
-
-    @NotBlank(message = "path must be not empty")
-    @Size(min = 1, max = 1024, message = "length path must be from 1 to 1024")
-    @Pattern(
-            regexp = "^([^/]+/)+$",
-            message = "Name folder should be as in the example: folder/"
-    )
+    @Size(min = 1, max = 1024, message = "Path length must be from 1 to 1024")
     private String path;
 }
+
