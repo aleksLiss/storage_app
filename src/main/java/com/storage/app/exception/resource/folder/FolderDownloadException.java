9d3b962b50780e94850b23038fd247600bc5a0c8
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-03 08:44:06 +0300

    add folder exception

---
+++ src/main/java/com/storage/app/exception/resource/folder/FolderDownloadException.java
@@ -0,0 +1,7 @@
+package com.storage.app.exception.resource.folder;
+
+public class FolderDownloadException extends RuntimeException {
+    public FolderDownloadException(String message) {
+        super(message);
+    }
+}
