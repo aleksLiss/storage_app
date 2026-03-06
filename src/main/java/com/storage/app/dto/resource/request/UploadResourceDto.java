476f32f14c9b441087a079650762bd0fa1365a6b
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-06 23:24:09 +0300

    add doc-api for dto

--- src/main/java/com/storage/app/dto/resource/request/UploadResourceDto.java
+++ src/main/java/com/storage/app/dto/resource/request/UploadResourceDto.java
@@ -1,10 +1,13 @@
 package com.storage.app.dto.resource.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(description = "Dto for upload resources")
 public class UploadResourceDto {
+    @Schema(description = "path to resource", example = "folder1/folder2/")
     @Size(min = 1, max = 1024, message = "Path length must be from 1 to 1024")
     private String path;
 }
--- src/main/java/com/storage/app/dto/user/UserDto.java
+++ src/main/java/com/storage/app/dto/user/UserDto.java
@@ -1,5 +1,6 @@
 package com.storage.app.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -10,11 +11,15 @@ import org.hibernate.validator.constraints.Length;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Dto for sign-up user")
 public class UserDto {
-    @NotBlank
-    @Length(message = "name must be not empty and has length 3 - 30", min=3, max=30)
+    @Schema(name = "email", examples = "max@google.com")
+    @NotBlank(message = "name must be not empty")
+    @Length(message = "name must be not empty and has length 5 - 30", min=5, max=30)
     @Email
     private String username;
-    @NotBlank @Length(message = "password must be not empty and has length 8 - 100", min=8, max=100)
+    @Schema(name = "password", examples = "123qwe")
+    @NotBlank(message = "password must be not empty")
+    @Length(message = "password must be not empty and has length 6 - 100", min=6, max=100)
     private String password;
 }
