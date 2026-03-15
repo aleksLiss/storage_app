19b48ffb8bf80d1c31ded65aa31d589bcf574d2f
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-15 13:49:05 +0300

    update dto

--- src/main/java/com/storage/app/dto/resource/request/MoveResourceDto.java
+++ src/main/java/com/storage/app/dto/resource/request/MoveResourceDto.java
@@ -1,6 +1,7 @@
 package com.storage.app.dto.resource.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -8,9 +9,11 @@ import lombok.Data;
 @Schema(description = "Dto for move or rename resource")
 public class MoveResourceDto {
     @Schema(description = "path from or old name of resource")
+    @NotBlank(message = "Path must be not empty")
     @Size(min = 1, max = 1024, message = "Path length must be from 1 to 1024")
     private String from;
     @Schema(description = "path to or new name of resource")
+    @NotBlank(message = "Path must be not empty")
     @Size(min = 1, max = 1024, message = "Path length must be from 1 to 1024")
     private String to;
 }
--- src/main/java/com/storage/app/dto/resource/request/UploadResourceDto.java
+++ src/main/java/com/storage/app/dto/resource/request/UploadResourceDto.java
@@ -1,6 +1,7 @@
 package com.storage.app.dto.resource.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -9,6 +10,7 @@ import lombok.Data;
 public class UploadResourceDto {
     @Schema(description = "path to resource", example = "folder1/folder2/")
     @Size(min = 1, max = 1024, message = "Path length must be from 1 to 1024")
+    @NotBlank(message = "Path must be not empty")
     private String path;
 }
 
