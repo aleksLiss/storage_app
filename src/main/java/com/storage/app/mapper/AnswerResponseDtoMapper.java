f4e69c8a51cbb717aed1d4b498cf44a520d7e9dd
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-15 20:11:42 +0300

    update mapper

--- src/main/java/com/storage/app/mapper/AnswerResponseDtoMapper.java
+++ src/main/java/com/storage/app/mapper/AnswerResponseDtoMapper.java
@@ -1,6 +1,7 @@
 package com.storage.app.mapper;
 
 import com.storage.app.dto.resource.response.AnswerResponseDto;
+import com.storage.app.security.UserPrincipal;
 import com.storage.app.util.resource.ResourceFinder;
 import io.minio.Result;
 import io.minio.messages.Item;
@@ -8,6 +9,7 @@ import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +22,8 @@ public interface AnswerResponseDtoMapper {
 
     default List<AnswerResponseDto> getListAnswerResponseDtos(Iterable<Result<Item>> results,
                                                               @Context ResourceFinder resourceFinder,
-                                                              String resource) {
+                                                              String resource,
+                                                              @AuthenticationPrincipal UserPrincipal userDetails) {
         List<AnswerResponseDto> response = new ArrayList<>();
         try {
             for (Result<Item> result : results) {
@@ -30,10 +33,13 @@ public interface AnswerResponseDtoMapper {
                 if (fullPath.equals(normalizedResource)) continue;
                 AnswerResponseDto answerDto = toDto(item);
                 String name = resourceFinder.getResourceNameFromPath(fullPath);
-                String path = resourceFinder.getPathToResourceFromPath(fullPath);
+                String userRootPrefix = String.format("user-%s-files/", userDetails.getUserId());
+                String relativePath = fullPath.startsWith(userRootPrefix)
+                        ? fullPath.substring(userRootPrefix.length())
+                        : fullPath;
                 boolean isDirectory = fullPath.endsWith("/");
                 answerDto.setName(name);
-                answerDto.setPath(path);
+                answerDto.setPath(relativePath);
                 answerDto.setType(isDirectory ? "DIRECTORY" : "FILE");
                 response.add(answerDto);
             }
